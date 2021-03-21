3a575c31aa88bd60f6c9d84c80bafcf250e6407d
Bruno Futema <bruno.futema@outlook.com>
2021-03-21 15:14:03 -0300

    feat: :rocket: React structure

--- react/chapter01/01-github-explorer/webpack.config.js
+++ react/chapter01/01-github-explorer/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -12,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
