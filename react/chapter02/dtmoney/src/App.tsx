c73af3cc4e828e00c047c7f1f2ba403bf593c815
Bruno Futema <bruno.futema@outlook.com>
2021-04-02 22:55:14 -0300

    feat: :sparkles: Componentization

--- react/chapter02/dtmoney/src/App.tsx
+++ react/chapter02/dtmoney/src/App.tsx
@@ -1,10 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div id="app">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
