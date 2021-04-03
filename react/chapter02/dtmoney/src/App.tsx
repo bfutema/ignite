0eb30e245e9a9d3cf43884cd07687958ee7a4397
Bruno Futema <bruno.futema@outlook.com>
2021-04-03 00:45:39 -0300

    feat: :sparkles: Contexts and hooks

--- react/chapter02/dtmoney/src/App.tsx
+++ react/chapter02/dtmoney/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
+import { TransactionsProvider } from './hooks/useTransactions';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
@@ -21,7 +23,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
