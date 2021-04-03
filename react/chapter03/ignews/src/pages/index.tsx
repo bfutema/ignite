d94301dfba81d8e00fd60920214b0fc9b622dc0a
Bruno Futema <bruno.futema@outlook.com>
2021-04-03 01:53:13 -0300

    feat: :sparkles: Application structure (ignews)

---
+++ react/chapter03/ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  );
+};
