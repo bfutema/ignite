ee6032c4005c5da13ef955bac8f59c1cfce7e405
Bruno Futema <bruno.futema@outlook.com>
2021-04-03 03:00:39 -0300

    feat: :sparkles: Integration with API

--- react/chapter03/ignews/src/pages/index.tsx
+++ react/chapter03/ignews/src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+
+import { stripe } from '../services/stripe';
+
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: { priceId: string; amount: number; };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,10 +24,10 @@ export default function Home() {
           <h1>New about <br /> the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -27,3 +35,22 @@ export default function Home() {
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ic27zBHJGZIiQPQkWo9lkjA', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+}
