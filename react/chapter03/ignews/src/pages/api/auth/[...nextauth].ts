e122dbf0e520bbb7470c95f53ab20969046aa7c1
Bruno Futema <bruno.futema@outlook.com>
2021-04-03 04:31:21 -0300

    feat: :sparkles: Using FaunaDB

--- react/chapter03/ignews/src/pages/api/auth/[...nextauth].ts
+++ react/chapter03/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,41 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  // jwt: {
+  //   signingKey: process.env.JWT_SIGNING_KEY,
+  // },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email),
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } },
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email),
+              ),
+            ),
+          ),
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  },
 });
