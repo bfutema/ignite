ff9cdf9a6c505ed7fc569930338991c45643dd24
Bruno Futema <bruno.futema@outlook.com>
2021-04-19 02:36:58 -0300

    feat: :tada: Challenge 01 chapter 02 completed :rocket:

---
+++ react/chapter02/_challenges/challenge01/src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { useCart } from '../../hooks/useCart';
+
+import logo from '../../assets/images/logo.svg';
+
+import { Container, Cart } from './styles';
+
+const Header = (): JSX.Element => {
+  const { cart } = useCart();
+  
+  const cartSize = cart.length;
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+
+          <span data-testid="cart-size">
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+          </span>
+        </div>
+
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+};
+
+export default Header;
