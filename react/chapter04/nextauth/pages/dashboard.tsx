71859f498745061d7b78e1856e540971eb367eac
Bruno Futema <bruno.futema@outlook.com>
2021-05-08 17:47:36 -0300

    feat: :rocket: Authentication and Authorization module

---
+++ react/chapter04/nextauth/pages/dashboard.tsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from 'react';
+
+import { Can } from '../components/Can';
+
+import { AuthContext } from '../contexts/AuthContext';
+
+import { setupAPIClient } from '../services/api';
+import { api } from '../services/apiClient';
+
+import { withSSRAuth } from '../utils/withSSRAuth';
+
+export default function Dashboard() {
+  const { user, isAuthenticated, signOut } = useContext(AuthContext);
+  
+  useEffect(() => {
+    api.get('/me')
+      .then(response => console.log(response));
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  );
+};
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  return {
+    props: {},
+  };
+});
---
+++ react/chapter04/nextauth/utils/withSSRAuth.ts
@@ -0,0 +1,63 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { destroyCookie, parseCookies } from 'nookies';
+import decode from 'jwt-decode';
+
+import { AuthTokenError } from '../services/errors/AuthTokenError';
+import { validateUserPermissions } from './validateUserPermissions';
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token'];
+
+    if (!cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[]; }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: true,
+          },
+        };
+      }
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token');
+        destroyCookie(ctx, 'nextauth.refreshToken');
+  
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        };
+      }
+    }
+  };
+}
